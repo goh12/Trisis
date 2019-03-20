9fd44854d8a66d0839d4cd9b6d9ae4c28be2e6a8
Frosti Grétarsson <frg17@hi.is>
2019-03-20 13:08:36 +0000

    Eyða drasli

---
+++ src/objects/entities/Triomino.js
@@ -0,0 +1,43 @@
+function Triomino() {
+    this.kubbar = [];
+
+}
+
+
+Triomino.prototype.update = function() {
+    
+}
+
+
+Triomino.prototype.render = function() {
+    
+}
+
+
+Triomino.prototype.rotateX = function() {
+    
+}
+
+
+Triomino.prototype.rotateY = function() {
+    
+}
+
+
+Triomino.prototype.rotateZ = function() {
+    
+}
+
+Triomino.prototype.moveX = function() {
+    
+}
+
+
+Triomino.prototype.moveZ = function() {
+    
+}
+
+
+Triomino.prototype.modeDown = function() {
+    
+}
